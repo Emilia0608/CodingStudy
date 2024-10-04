67fbd53b8fa9e1ebd94fcec9015cabb104affd3f
Emilia.jy <98992915+JiyoonLee18@users.noreply.github.com>
2024-10-04 19:06:00 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 176 ms, Memory: 35756 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.py
@@ -0,0 +1,23 @@
+N, K, B=list(map(int,input().split()))
+check= [0] * N
+for _ in range(B):
+    broken=int(input())-1
+    check[broken]=1
+
+psum = [0] * N
+psum[0] = check[0]
+
+for i in range(1, N):
+    psum[i] = psum[i-1] + check[i]
+
+need=[]
+for i in range(0, N-K+1):
+    # i ~ i + K - 1
+    if i == 0:
+        sum = psum[i+K-1]
+    else:
+        sum= psum[i+K-1]-psum[i-1]
+    
+    need.append(sum)
+
+print(min(need))
