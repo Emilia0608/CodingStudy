4777068e7ca93d8b67ab956bcb9e683a55a3383b
Emilia.jy <98992915+Emilia0608@users.noreply.github.com>
2025-04-17 20:34:18 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,18 @@
+case=int(input())
+for _ in range(case):
+    n=int(input())
+    answer=1 # 경우의 수를 곱할 것이기에 초깃값 1 설정
+
+    clothes={}
+    for i in range(n):
+        cloth=input()
+        if cloth.split(" ")[1] not in clothes.keys(): # 종류 처음일 때
+            clothes[cloth.split(" ")[1]]=1
+        else:
+            clothes[cloth.split(" ")[1]]+=1
+
+    for keys in clothes.keys():
+        answer*=(clothes[keys]+1) # 안 입는 경우 +1
+    print(answer-1) # 다 안 입는 경우 -1
+
+    
